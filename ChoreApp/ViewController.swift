9779131f4cec773d528586dadab5fd9409d37394
Mackenzie <mackenziehampel@weber.edu>
2019-07-20 17:06:18 -0600

    Initial Commit

---
+++ ChoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChoreApp
+//
+//  Created by Mackenzie Hampel on 7/20/19.
+//  Copyright © 2019 Mackenzie Hampel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
